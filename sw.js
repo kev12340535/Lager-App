88fbf463777bbdbc92016305d72f3e61783f983b
kev12340535 <kevin.Ilmenau@gmail.com>
2025-11-15 20:32:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,30 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('lager-cache').then(cache => cache.addAll(['./','./index.html'])));
+const CACHE_NAME = 'lager-einfach-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
 });
